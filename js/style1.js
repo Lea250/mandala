079606b0457282f078861acc8b1cdf5c76ca8871
Elena <lea250@gmail.com>
2017-11-15 17:13:38 +0000

    third try to push eveything

---
+++ js/style1.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+   $('№pg2').click(function(){
+       
+     $ ('№pg2').css('border', '2px solid green');
+     }) ;
+     
+     $('#slideDown').click(function(){
+         $('pic5-2').draggble();
+     });
+     
+});
